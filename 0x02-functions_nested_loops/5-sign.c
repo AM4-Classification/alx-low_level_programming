8237e1a20f9be5b5ba56ac347e817ef034541bc5
Thabang M <Martinthabang@mail.com>
2023-07-19 16:13:49 -0700

    Betty style coments with *

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- print_sign - prints the sign of a number
- Return: 1 and prints + if n is greater than zero
- 0 and prints 0 if n is zero,
- prints -1 and prints - if n is less than zero
+ * print_sign - prints the sign of a number
+ *Return: 1 and prints + if n is greater than zero
+ *0 and prints 0 if n is zero,
+ *prints -1 and prints - if n is less than zero
  */
 int print_sign(int n)
 {
