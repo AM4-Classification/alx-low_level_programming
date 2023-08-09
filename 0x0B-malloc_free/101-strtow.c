e7f85d6dc4755c5e459879039ec75cb32082ec95
Thabang M <Martinthabang@mail.com>
2023-08-09 13:24:18 -0700

    101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -7,15 +7,13 @@
  * @s: The string to evaluate.
  * Return: The number of words.
  */
-int count_word(char *s)
-{
+int count_word(char *s) {
   int flag, char_count, word_count;
 
   flag = 0;
   word_count = 0;
 
-  for (char_count = 0; s[char_count] != '\0'; char_count++ 
-  {
+  for (char_count = 0; s[char_count] != '\0'; char_count++) {
     if (s[char_count] == ' ')
       flag = 0;
     else if (flag == 0) {
@@ -32,20 +30,19 @@ int count_word(char *s)
  * @str: The string to split.
  * Return: Pointer to an array of strings (Success) or NULL (Error).
  */
-char ** strtow(char *str)
-{
+char **strtow(char *str) {
   char **word_matrix, *tmp;
-  int i, k = 0, len = 0, word_count, char_count = 0, start, end;
+  int i, k = 0, len = 0, words, char_count = 0, start, end;
 
   while ( *(str + len))
     len++;
 
-  word_count = count_word(str);
+  words = count_word(str);
 
-  if (word_count == 0)
+  if (words == 0)
     return (NULL);
 
-  word_matrix = (char **) malloc(sizeof(char *) *(word_count + 1));
+  word_matrix = (char **) malloc(sizeof(char *) *(words + 1));
 
   if (word_matrix == NULL)
     return (NULL);
@@ -54,14 +51,13 @@ char ** strtow(char *str)
     if (str[i] == ' ' || str[i] == '\0') {
       if (char_count) {
         end = i;
-        tmp = (char *) malloc(sizeof(char) *(char_count + 1));
+        tmp = (char * ) malloc(sizeof(char) * (char_count + 1));
 
         if (tmp == NULL)
           return (NULL);
 
         while (start < end)
           *tmp++ = str[start++];
-
         *tmp = '\0';
         word_matrix[k] = tmp - char_count;
         k++;
