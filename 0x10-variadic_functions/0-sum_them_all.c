239c8a43e651d7da0039ae5ab051c47c5bf45e6d
Thabang M <Martinthabang@mail.com>
2023-08-17 18:23:35 -0700

    fixed betty on 0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -2,24 +2,24 @@
 #include <stdarg.h>
 
 /**
- * sum_them_all - Returns the sum of all its parameters.
- * @param_count: The number of parameters passed to the function.
- * @...: A variable number of parameters to calculate the sum of.
+ * sum_them_all - Returns the sum of all its paramters.
+ * @n: The number of paramters passed to the function.
+ * @...: A variable number of paramters to calculate the sum of.
  *
- * Return: If param_count == 0 - 0.
+ * Return: If n == 0 - 0.
  *         Otherwise - the sum of all parameters.
  */
-int sum_them_all(const unsigned int param_count, ...)
+int sum_them_all(const unsigned int n, ...)
 {
-    va_list arguments;
-    unsigned int i, sum = 0;
+	va_list ap;
+	unsigned int i, sum = 0;
 
-    va_start(arguments, param_count);
+	va_start(ap, n);
 
-    for (i = 0; i < param_count; i++)
-        sum += va_arg(arguments, int);
+	for (i = 0; i < n; i++)
+		sum += va_arg(ap, int);
 
-    va_end(arguments);
+	va_end(ap);
 
-    return (sum);
+	return (sum);
 }
